465d6bc889979fded13a98d214f6c28ca3966b6e
kelleman <kellemanatser@gmail.com>
2022-11-05 13:04:24 -0700

    done

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,65 +1,73 @@
-#include <stdio.h>
 #include "main.h"
+#include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
-/**
-  *main - entry
-  *@argv : the string from the terminal
-  *@argc : the number of arameters
-  *Return: 0
-  */
+#include <stdbool.h>
 
-int main(int argc, char const *argv[])
+/**
+ * coinConverter - Helper function that does all the mathematics
+ * @i: Passed in variable from main for calculations
+ * Return: The number of coins needed minimum for the passed in variable
+ */
+int coinConverter(int i)
 {
+	int count = 0;
+
+	while (i != 0)
+	{
+		if (i % 10 == 9 || i % 10 == 7)
+			i -= 2;
+		else if (i % 25 == 0)
+			i -= 25;
+		else if (i % 10 == 0)
+			i -= 10;
+		else if (i % 5 == 0)
+			i -= 5;
+			else if (i % 2 == 0)
+		{
+			if (i % 10 == 6)
+				i -= 1;
+			else
+				i -= 2;
+		}
+		else
+			i -= 1;
+
+		count++;
+	}
+
+	return (count);
+}
+
+/**
+ * main - Takes in exactly one argument for minimum coin count
+ * @argc: Number of command line arguments
+ * @argv: Array name
+ * Return: 0 if exactly 1 argument is passed into this program, 1 otherwise
+ */
+int main(int argc, char *argv[])
+{
+	int i, coin;
+
+	coin = 0;
+
 	if (argc != 2)
 	{
 		printf("Error\n");
-		return (-1);
+		return (1);
 	}
-	if (atoi(argv[1]) >= 0)
-	{
-		int c = 0;
 
-		checker(atoi(argv[1]), c);
-	}
-	else if (atoi(argv[1]) < 0)
-	{
+	i = atoi(argv[1]);
+
+	if (i < 0)
 		printf("0\n");
+
+	else
+	{
+		coin = coinConverter(i);
+
+		printf("%d\n", coin);
 	}
+
 	return (0);
 }
-
-/**
-  * checker - entry function
-  *@c : variable
-  *@b : variable
-  */
-
-void checker(int c, int b)
-{
-	if (c >= 25)
-	{
-		checker(c - 25, b + 1);
-	}
-	else if (c >= 10)
-	{
-		checker(c - 10, b + 1);
-	}
-	else if (c >= 5)
-	{
-		checker(c - 5, b + 1);
-	}
-	else if (c >= 2)
-	{
-		checker(c - 2, b + 1);
-	}
-	else if (c >= 1)
-	{tom
-		checker(c - 1, b + 1);
-	}
-	else if (c == 0)
-	{
-		printf("%d\n", b);
-	}
-}
-
